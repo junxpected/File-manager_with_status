a4d8b657f7e455d6a74b01ca2bf0f1e5e7a77f87
junxpected <megasharik90@gmail.com>
2026-03-06 16:01:12 +0200

    correct MediaAsset

--- File-manager/Models/MediaAsset.cs
+++ File-manager/Models/MediaAsset.cs
@@ -1,9 +1,10 @@
 ﻿using System.IO;
 using File_manager.Models;
+using File_manager.Interfaces;
 
-namespace MediaAsset
+namespace File_manager.Models
 {
-    public class MediaAsset
+    public class MediaAsset : IAsset
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string FullPath { get; set; } = string.Empty;
